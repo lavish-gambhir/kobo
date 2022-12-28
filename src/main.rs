084086c31092a1b4dff297db709c5207ce0fa19e
Lavish Gambhir <lavish_gambhir@hotmail.com>
2022-12-28 19:09:36 +0530

    Refactor telemetry mod

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
-use kobo::configuration::get_configuration;
-use sqlx::{Connection, PgPool};
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{EnvFilter, Registry};
 
-use kobo::startup;
+use sqlx::{Connection, PgPool};
+
+use kobo::configuration::get_configuration;
+use kobo::{startup, telemetry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("kobo".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    telemetry::init_subscriber(telemetry::get_subscriber("kobo".into(), "info".into()));
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let addr = format!("127.0.0.1:{}", configuration.application_port);
