f4d765d31d039432518668b5e4d999c10b60866c
Lavish Gambhir <lavish_gambhir@hotmail.com>
2022-12-29 13:30:12 +0530

    Added  to take care of request id gen

--- src/startup.rs
+++ src/startup.rs
@@ -6,13 +6,14 @@ use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
     // wrapping the `connection` in a smart pointer
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool.clone())
