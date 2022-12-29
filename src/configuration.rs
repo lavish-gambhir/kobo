dbbeb62be49ad9aa89f263e03574b0e2ac282c8d
Lavish Gambhir <lavish_gambhir@hotmail.com>
2022-12-29 19:31:56 +0530

    Added validation fn for subscriber name

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,6 +3,7 @@
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 use sqlx::postgres::PgConnectOptions;
+use sqlx::ConnectOptions;
 
 #[derive(Deserialize)]
 pub struct Settings {
@@ -27,7 +28,9 @@ pub struct ApplicationSettings {
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use actix_web::{web, HttpResponse};
 use serde::Deserialize;
 use sqlx::types::chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -20,6 +21,9 @@ subscriber_name = % form.name
 )
 )]
 pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subscriber(pool.as_ref(), &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -48,4 +52,12 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
         e
     })?;
     Ok(())
+}
+
+fn is_valid_name(name: &str) -> bool {
+    let is_empty_or_whitespace = name.trim().is_empty();
+    let is_too_long = name.graphemes(true).count() > 256;
+    let forbidden_chars = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_chars = name.chars().any(|c| forbidden_chars.contains(&c));
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_chars)
 }
