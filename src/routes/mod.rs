0a8707d152d31c479fe118619bd8d78a74a470b1
Lavish Gambhir <lavish_gambhir@hotmail.com>
2023-01-12 10:36:59 +0530

    Added homepage "/" route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,11 @@
 mod health;
+mod home;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use health::*;
+pub use home::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
